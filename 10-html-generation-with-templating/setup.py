87c7173f27f8c77ecc4618cc621da32af12446af
rbhatta <raj@sstwireless.com>
2021-09-09 16:43:14 -0700

    html generation with template

---
+++ 10-html-generation-with-templating/setup.py
@@ -0,0 +1,17 @@
+from setuptools import  setup
+requires =[
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_chameleon',
+    'waitress'
+]
+
+setup(
+    name='tutorial',
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = tutorial:main'
+        ],
+    },
+)
--- 10-pyramid-package/mysite/views.py
+++
@@ -1,6 +0,0 @@
-from pyramid.view import view_config
-
-@view_config(route_name='list',
-             renderer='templates/list.jinja2')
-def list(request):
-    return dict()
