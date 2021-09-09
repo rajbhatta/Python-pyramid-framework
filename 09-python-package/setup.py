3a52d502a853939848e300c2429438c60c1606e2
rbhatta <raj@sstwireless.com>
2021-09-09 16:21:56 -0700

    Python package for pyramid is modified

--- 09-python-package/setup.py
+++ 09-python-package/setup.py
@@ -1,11 +1,13 @@
 from setuptools import  setup
 requires =[
     'pyramid',
-    'pyramid_jinja2'
+    'pyramid_jinja2',
+    'waitress'
 ]
-setup(name='mysite',
+setup(name='tutorial',
       install_requires=requires,
-      entry_points="""\
-        [paste.app_factory]
-        main = mysite:main
-        """)
+      entry_points={
+          'paste.app_factory': [
+              'main = tutorial:main'
+          ],
+      })
